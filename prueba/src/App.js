30a7a63781820ed1e1cc06a7098072b3f3876982
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-12-09 16:42:07 -0500

    adding all

---
+++ prueba/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
---
+++ prueba/src/components/SignUp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SignUp() {
+
+    const style = {
+        background: 'linear-gradient(to right, #261089, #533eab, #6674ff, #00dfc3)'  
+    }
+
+    return (
+        <section className="bg- h-full pt-10 md:pt-20 md:h-screen items-center justify-center">
+            <div className="container py-1 rounded-lg max-w-lg mx-auto text-center">
+                mateo
+            </div>
+        </section>
+    )
+}
+
+export default SignUp
